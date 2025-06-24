7da7e706f0d048ce137f2f9a33a9c4eb750b35e4
Hariharn <harigj14@gmail.com>
2025-06-24 16:32:00 +0530

    Final commit

--- StrategyPattern/ViewController.swift
+++ StrategyPattern/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let objCreditcardPayment = CreditcardPayment()
+    let objGpayPayment = GpayPayment()
+    let objPhonepayPayment = PhonepayPayment()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let objPaymentContext = PaymentContext(objpaymentStrategy: objCreditcardPayment)
+
+        objPaymentContext.payBill(amount: 231)
+
+        objPaymentContext.setStrategy(objGpayPayment)
+        
+        objPaymentContext.payBill(amount: 33)
+        
+        objPaymentContext.setStrategy(objPhonepayPayment)
+        
+        objPaymentContext.payBill(amount: 780)
     }
 
 
@@ -63,12 +81,12 @@ class PaymentContext{
     }
     
     func setStrategy(_ strategy: paymentStrategy) {
-            self.objpaymentStrategy = strategy
-        }
-        
-        func payBill(amount: Double) {
-            objpaymentStrategy.Pay(amount: amount)
-        }
+        self.objpaymentStrategy = strategy
+    }
+    
+    func payBill(amount: Double) {
+        objpaymentStrategy.Pay(amount: amount)
+    }
     
 }
 
